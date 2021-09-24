baec3eaabd8d6fe5b01724f479c6db163b3f3a0b
buscanopaul <buscano.paul@gmail.com>
2021-09-24 13:58:23 +0800

    Change index near

--- app/controllers/near_controller.rb
+++ app/controllers/near_controller.rb
@@ -1,15 +1,9 @@
 class NearController < ApplicationController
   def index
-    @response = Faraday.get("http://ip-api.com/json")
-    @response_result = JSON.parse(@response.body)
-    @region = @response_result['regionName']
-    @city = @response_result['city']
-
-    # @cars = Car.find_by(city: @city, region: @region)
-    @cars = Car.where('city LIKE ?', "%#{@city}%").where('region LIKE ?', "%#{@region}%")
     
+    @city = request.location.city
+    @region = request.location.region
 
-    @cityTest = request.location.city
-    @regionTest = request.location.region
+    @cars = Car.where('city LIKE ?', "%#{@city}%").where('region LIKE ?', "%#{@region}%")
   end
 end
