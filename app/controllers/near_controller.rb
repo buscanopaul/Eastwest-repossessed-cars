4c800372bca42d754d4b08fbf61819e763fa5f32
buscanopaul <buscano.paul@gmail.com>
2021-09-25 11:21:27 +0800

    test location

--- app/controllers/near_controller.rb
+++ app/controllers/near_controller.rb
@@ -3,6 +3,9 @@ class NearController < ApplicationController
     
     @city = request.location.city
     @region = request.location.region
+    @latitude = request.location.latitude
+    @longitude = request.location.longitude
+    @location = request.location
 
     @cars = Car.where('city LIKE ?', "%#{@city}%").where('region LIKE ?', "%#{@region}%")
   end
